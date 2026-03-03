c942560f219cf08bd9c0934b73537fbd6128a5c5
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-03-03 17:08:58 +0100

    add HelloWebDriver task with basic Selenium WebDriver setup

---
+++ src/main/java/com/epam/training/student_anna_pienkowska/first_code/HelloWebDriver.java
@@ -0,0 +1,30 @@
+package com.epam.training.student_anna_pienkowska.first_code;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWebDriver {
+    public static void main(String[] args) throws InterruptedException {
+
+        // Initialize
+        WebDriver driver = new ChromeDriver();
+        // Navigation
+        driver.get("http://seleniumhq.org");
+
+//        Other drivers
+//        driver = new FirefoxDriver();
+//        driver.get("http://seleniumhq.org");
+//        Thread.sleep(2000);
+//
+//        driver = new EdgeDriver();
+//        driver.get("http://seleniumhq.org");
+//        Thread.sleep(2000);
+
+        // Close
+        driver.quit();
+    }
+}
