35b823e9532892a1fbf7d114d8739400ebce138d
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-03-25 17:19:02 +0100

    fix BasePage and add LoginPage

--- src/main/java/com/epam/training/student_anna_pienkowska/final_task/pages/BasePage.java
+++ src/main/java/com/epam/training/student_anna_pienkowska/final_task/pages/BasePage.java
@@ -1,6 +1,7 @@
 package com.epam.training.student_anna_pienkowska.final_task.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
@@ -12,12 +13,12 @@ public abstract class BasePage {
         PageFactory.initElements(driver, this);
     }
 
-    protected void set(By locator, String value) {
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(value);
+    protected void set(WebElement element, String value) {
+        element.clear();
+        element.sendKeys(value);
     }
 
-    protected void click(By locator) {
-        driver.findElement(locator).click();
+    protected void click(WebElement element) {
+        element.click();
     }
 }
---
+++ src/main/java/com/epam/training/student_anna_pienkowska/final_task/pages/LoginPage.java
@@ -0,0 +1,40 @@
+package com.epam.training.student_anna_pienkowska.final_task.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(id = "user-name")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//h3[@data-test='error']")
+    private WebElement errorMessage;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public LoginPage setUsername(String username) {
+        set(usernameField, username);
+        return this;
+    }
+
+    public LoginPage setPassword(String password) {
+        set(passwordField, password);
+        return this;
+    }
+
+    public void clickLoginButton() {
+        click(loginButton);
+    }
+
+}
