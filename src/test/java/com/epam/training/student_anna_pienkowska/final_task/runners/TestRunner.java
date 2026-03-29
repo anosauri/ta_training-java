2f6d8948651ac84d93b5c3fdaf21a5a60c371818
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-03-29 16:05:05 +0200

    setup Allure reports and update .gitignore

--- src/test/java/com/epam/training/student_anna_pienkowska/final_task/runners/TestRunner.java
+++ src/test/java/com/epam/training/student_anna_pienkowska/final_task/runners/TestRunner.java
@@ -6,7 +6,8 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
         features = "src/test/resources/final_task/features",
         glue = "com.epam.training.student_anna_pienkowska.final_task.steps",
-        monochrome = true
+        monochrome = true,
+        plugin = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
