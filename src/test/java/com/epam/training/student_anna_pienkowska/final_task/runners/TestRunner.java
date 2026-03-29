8cee2dfd06ba992f5af9d3cd8f8a047967ff09fc
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-03-29 16:01:31 +0200

    configure parallel run for Cucumber scenarios

--- src/test/java/com/epam/training/student_anna_pienkowska/final_task/runners/TestRunner.java
+++ src/test/java/com/epam/training/student_anna_pienkowska/final_task/runners/TestRunner.java
@@ -1,6 +1,7 @@
 package com.epam.training.student_anna_pienkowska.final_task.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/final_task/features",
@@ -9,5 +10,9 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
