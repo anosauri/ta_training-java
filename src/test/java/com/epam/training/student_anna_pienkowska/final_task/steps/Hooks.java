fd9164060426c1959b8281ace6ba8093d23e0ffc
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-04-07 23:20:27 +0200

    remove empty method

--- src/test/java/com/epam/training/student_anna_pienkowska/final_task/steps/Hooks.java
+++ src/test/java/com/epam/training/student_anna_pienkowska/final_task/steps/Hooks.java
@@ -2,15 +2,8 @@ package com.epam.training.student_anna_pienkowska.final_task.steps;
 
 import com.epam.training.student_anna_pienkowska.final_task.drivers.DriverManager;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 
 public class Hooks {
-
-    @Before
-    public void setup() {
-        //TODO What's the point of this method? It doesn't do anything.
-    }
-
     @After
     public void tearDown() {
         DriverManager.closeDriver();
